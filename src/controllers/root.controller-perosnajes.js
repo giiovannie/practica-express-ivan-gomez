7df11fa85dff85b42af584a9042752d09d2dff6b
gomez ivan <ivanemmanuelgomez2017@gmail.com>
2026-06-13 23:52:17 -0300

    feat: se agreg una funcion que  recibe un informacion y lko inserta en la pseudo-base de datos

--- src/controllers/root.controller-perosnajes.js
+++ src/controllers/root.controller-perosnajes.js
@@ -2,16 +2,16 @@
 
 import { herosPersonajes } from "../data/perosnajes.js";
 
+let errorClient = {
+  message: "error en la solicitud",
+};
+
 export const traerPersonajes = (req, res) => {
   res.json(herosPersonajes);
 };
 
 export const traerPersonajesID = (req, res) => {
   let objetoReqParams = Number(req.params.id);
-
-  let errorClient = {
-    message: "error en la solicitud",
-  };
 
   if (objetoReqParams < 0 || isNaN(objetoReqParams)) {
     return res.status(400).json(errorClient)
@@ -27,3 +27,30 @@ export const traerPersonajesID = (req, res) => {
     }
   }  
 };
+
+
+export const agregarPersonaje = (req,res)=>{
+  
+  const body = req.body;
+  console.log(req.body);
+
+
+  
+  if(body.nombre === undefined || body.imagen === undefined){
+    return res.status(400).json(errorClient);
+  }else{
+    const nombre = req.body.nombre;
+    const imagen = req.body.imagen;
+
+    let inyeccionReq = {
+      id: herosPersonajes[herosPersonajes.length - 1].id + 1,
+      nombre,
+      imagen
+    }
+
+    herosPersonajes.push(inyeccionReq)
+    console.log(herosPersonajes)
+
+    return res.json(herosPersonajes)
+  }
+}
--- src/routers/root.router-personajes.js
+++ src/routers/root.router-personajes.js
@@ -1,8 +1,10 @@
 //routers
 import { Router } from "express";
-import { traerPersonajes, traerPersonajesID } from "../controllers/root.controller-perosnajes.js";
+import { agregarPersonaje, traerPersonajes, traerPersonajesID } from "../controllers/root.controller-perosnajes.js";
 
 export const herosPersonajesRouter = Router()
 
 herosPersonajesRouter.get("/", traerPersonajes)
 herosPersonajesRouter.get("/:id", traerPersonajesID )
+
+herosPersonajesRouter.post("/new", agregarPersonaje)
